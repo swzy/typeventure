98eb887244562bd9c4bf5dbc7ad7ea435d0a6749
LampuKuroro <37754715+LampuKuroro@users.noreply.github.com>
2018-03-24 19:19:53 -0400

    Add files via upload

---
+++ StartCountdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartCountdown : MonoBehaviour {
+	float timeLeft = 5f;
+	public Health playerHealth;
+	public Slider Timer;
+	void StartOver(){
+		timeLeft = 5f;
+	}
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		Timer.value = timeLeft;
+		if(timeLeft <= 0)
+		{
+			playerHealth.TakeDamage(10);
+			timeLeft += 5f;
+		}
+	
+		
+	}
+	
+
+}
